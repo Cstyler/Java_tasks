85f544d8c59fa25f56be38c1c117973e444f41d7
Cstyler <hanchik97@gmail.com>
2016-04-09 22:48:53 +0300

    commit

--- src/Econom.java
+++ src/Econom.java
@@ -1,54 +1,49 @@
-import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
-class Econom {
-    private static ArrayList<String> stack;
+public class Econom {
+	private static Set<String> stringSet = new HashSet<>();
 
-    public static void main(String[] args) {
-        Scanner scn = new Scanner(System.in);
-        String s = scn.next();
-        stack = new ArrayList<>();
-        if (s.length() == 1) {
-            System.out.println(0);
-            return;
-        }
-        parseRec(s);
-        System.out.println(stack.size() + 1);
-    }
+	public static void main(String[] args) {
+		String s = new Scanner(System.in).next();
+		if (s.length() == 1) {
+			System.out.println(0);
+			return;
+		}
+		parseRec(s);
+		System.out.println(stringSet.size() + 1);
+	}
 
-    private static String parseRec(String s) {
-        if (s.length() == 1) {
-            return s;
-        }
-        String firstToken, secondToken;
-        if (s.charAt(2) == '(') {
-            int i = 3;
-            for (int pCount = 1; ; i++) {
-                if (s.charAt(i) == '(') {
-                    pCount++;
-                } else if (s.charAt(i) == ')') {
-                    pCount--;
-                }
-                if (pCount == 0) {
-                    break;
-                }
-            }
-            firstToken = s.substring(2, i + 1);
-            if (s.charAt(i + 1) == '(') {
-                secondToken = s.substring(i + 1, s.length() - 1);
-            } else {
-                secondToken = s.substring(i + 1, i + 2);
-            }
-        } else {
-            firstToken = s.substring(2, 3);
-            secondToken = s.charAt(3) == '(' ? s.substring(3, s.length() - 1) : s.substring(3, 4);
-        }
-        if (firstToken.length() != 1 && stack.indexOf(firstToken) == -1) {
-            stack.add(firstToken);
-        }
-        if (secondToken.length() != 1 && stack.indexOf(secondToken) == -1) {
-            stack.add(secondToken);
-        }
-        return parseRec(firstToken) + parseRec(secondToken);
-    }
+	private static String parseRec(String s) {
+		if (s.length() == 1) {
+			return s;
+		}
+		String firstToken, secondToken;
+		if (s.charAt(2) == '(') {
+			int i = skip(3, s);
+			firstToken = s.substring(2, i + 1);
+			secondToken = s.charAt(i + 1) == '(' ? s.substring(i + 1, s.length() - 1) : s.substring(i + 1, i + 2);
+		} else {
+			firstToken = s.substring(2, 3);
+			secondToken = s.charAt(3) == '(' ? s.substring(3, s.length() - 1) : s.substring(3, 4);
+		}
+		if (firstToken.length() != 1) {
+			stringSet.add(firstToken);
+		}
+		if (secondToken.length() != 1) {
+			stringSet.add(secondToken);
+		}
+		return parseRec(firstToken) + parseRec(secondToken);
+	}
+
+	private static int skip(int i, String s) {
+		for (int parenCount = 1; ; i++) {
+			parenCount += s.charAt(i) == '(' ? 1 : s.charAt(i) == ')' ? -1 : 0;
+			if (parenCount == 0) {
+				break;
+			}
+		}
+		return i;
+	}
 }
